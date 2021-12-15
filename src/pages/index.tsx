f09146292590a45d44e594127a0cdc0d8886debf
kota <kota200016@icloud.com>
2021-12-15 12:10:47 +0900

    トップページのテンプレート化

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,23 +7,26 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import { auth } from "@libs/firebaseConfig";
 
+//components
+import { TopPageTemplate } from "@components/templates/TopPageTemplate";
+
 const Home: NextPage = () => {
     const router = useRouter();
-
     // const [authState, setAuthState] = useState<undefined | firebase.User | null>(undefined);
 
     useEffect(() => {
         auth.onAuthStateChanged((user) => {
             // setAuthState(user);
-            !user && router.push("/login");
-            !user.emailVerified && router.push("/login");
+            if (!user || !user.emailVerified) {
+                router.push("/login");
+            }
         });
     }, [router]);
 
     const handleLogOut = async () => {
         try {
             await auth.signOut();
-            router.push("login");
+            router.push("/login");
         } catch (error) {
             alert(error.message);
         }
@@ -31,15 +34,7 @@ const Home: NextPage = () => {
 
     return (
         <div>
-            <main className="w-full mt-16  mb-16 pt-6 pb-6 border-2   lg:mt-20 ">
-                <h1 className="text-4xl text-center text-green-400">
-                    ここに求人投稿の一覧を表示する
-                </h1>
-
-                <p onClick={handleLogOut} className="p-3 rounded-md ml-4 inline-block bg-green-400">
-                    ログアウト
-                </p>
-            </main>
+            <TopPageTemplate handleLogOut={handleLogOut} />
         </div>
     );
 };
