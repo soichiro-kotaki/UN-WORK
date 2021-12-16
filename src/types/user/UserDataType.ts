48091705b7717b26ddebb09135219dc172cef163
kota <kota200016@icloud.com>
2021-12-16 23:58:08 +0900

    ユーザーデータの型修正

--- src/types/user/UserDataType.ts
+++ src/types/user/UserDataType.ts
@@ -1,7 +1,8 @@
 export type UserDataType = {
+    created_at: string;
     user_name: string;
     user_email: string;
     user_grade: "1年生" | "2年生" | "3年生" | "4年生";
     user_subject: "GM" | "こども" | "食健康";
-    user_mg: string;
+    user_img: string;
 };
