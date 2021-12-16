38e65b3bf78ca20976635bc6ccb79ca288ad7142
kota <kota200016@icloud.com>
2021-12-16 23:57:00 +0900

    レイアウトコンポーネントUIの修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     purge: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "background-main": "#f4f5f7",
+            },
+        },
     },
     variants: {
         extend: {},
