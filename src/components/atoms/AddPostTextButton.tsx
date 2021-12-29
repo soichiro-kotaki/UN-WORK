66424cbc9ff432074622ec224d9fe21cc994645d
kota <kota200016@icloud.com>
2021-12-29 18:23:15 +0900

    スタイル修正

--- src/components/atoms/AddPostTextButton.tsx
+++ src/components/atoms/AddPostTextButton.tsx
@@ -5,7 +5,7 @@ export const AddPostTextButton: React.FC = () => {
     return (
         <>
             <Link href="/postform">
-                <a className="text-gray-50 text-center block bg-green-400 hover:bg-green-700 transition-colors rounded-full lg:p-4">
+                <a className="text-gray-50 text-center block bg-green-400 hover:bg-green-700 transition-colors rounded-full lg:p-4 lg:shadow-xl">
                     求人を投稿する
                 </a>
             </Link>
--- src/components/layouts/MenuBar.tsx
+++ src/components/layouts/MenuBar.tsx
@@ -18,36 +18,36 @@ export const MenuBar: React.FC = () => {
             <div className="z-10 fixed bottom-0 bg-white border-gray-300 border-t w-full h-12 lg:left-0 lg:top-0 lg:w-1/5 lg:h-full lg:border-r lg:border-t-0">
                 <div>
                     <Link href="/">
-                        <a className="text-xs w-1/4 h-full text-center pt-2 inline-block md:w-1/6 lg:block lg:w-4/5 lg:h-16 lg:mt-16 lg:mx-auto lg:pt-2 lg:text-lg hover:bg-gray-200 lg:rounded-full">
-                            <AiFillHome className="w-5 h-5 text-center mx-auto  lg:w-8 lg:h-6" />
+                        <a className="text-xs w-1/4 h-full text-center pt-2 inline-block md:w-1/6 lg:block lg:w-4/5 lg:h-16 lg:mt-12 lg:mx-auto lg:pt-5 lg:text-lg hover:bg-gray-200 lg:rounded-full">
+                            <AiFillHome className="w-5 h-5 text-center mx-auto lg:inline-block lg:mr-3" />
                             ホーム
                         </a>
                     </Link>
 
                     <Link href="/announce">
-                        <a className="text-xs w-1/4 h-full text-center pt-2 inline-block md:w-1/6 lg:block lg:w-4/5 lg:h-16 lg:mt-16 lg:mx-auto lg:pt-2 lg:text-lg hover:bg-gray-200 lg:rounded-full">
-                            <MdAnnouncement className="w-5 h-5 text-center mx-auto" />
+                        <a className="text-xs w-1/4 h-full text-center pt-2 inline-block md:w-1/6 lg:block lg:w-4/5 lg:h-16 lg:mt-12 lg:mx-auto lg:pt-5  lg:text-lg hover:bg-gray-200 lg:rounded-full">
+                            <MdAnnouncement className="w-5 h-5 text-center mx-auto lg:inline-block lg:mr-3" />
                             お知らせ
                         </a>
                     </Link>
 
                     <Link href="/categories">
-                        <a className="text-xs w-1/4 h-full text-center pt-2 inline-block md:w-1/6 lg:block lg:w-4/5 lg:h-16 lg:mt-16 lg:mx-auto lg:pt-2 lg:text-lg hover:bg-gray-200 lg:rounded-full">
-                            <AiFillTags className="w-5 h-5 text-center mx-auto " />
+                        <a className="text-xs w-1/4 h-full text-center pt-2 inline-block md:w-1/6 lg:block lg:w-4/5 lg:h-16 lg:mt-12 lg:mx-auto lg:pt-5  lg:text-lg hover:bg-gray-200 lg:rounded-full">
+                            <AiFillTags className="w-5 h-5 text-center mx-auto lg:inline-block lg:mr-3" />
                             カテゴリー
                         </a>
                     </Link>
 
                     <Link href={`/user/${User.uid}`}>
-                        <a className="text-xs w-1/4 h-full text-center pt-2 inline-block md:w-1/6 lg:block lg:w-4/5 lg:h-16 lg:mt-16 lg:mx-auto lg:pt-2 lg:text-lg hover:bg-gray-200 lg:rounded-full">
-                            <FaUserAlt className="w-5 h-5 text-center mx-auto" />
+                        <a className="text-xs w-1/4 h-full text-center pt-2 inline-block md:w-1/6 lg:block lg:w-4/5 lg:h-16 lg:mt-12 lg:mx-auto lg:pt-5  lg:text-lg hover:bg-gray-200 lg:rounded-full">
+                            <FaUserAlt className="w-5 h-5 text-center mx-auto lg:inline-block lg:mr-3" />
                             マイページ
                         </a>
                     </Link>
                 </div>
 
                 {/* PC版の求人投稿ページ遷移ボタン */}
-                <div className="hidden lg:block lg:w-4/5 lg:mx-auto lg:mt-16">
+                <div className="hidden lg:block lg:w-4/5 lg:mx-auto lg:mt-20">
                     <AddPostTextButton />
                 </div>
             </div>
