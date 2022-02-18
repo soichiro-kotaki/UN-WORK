4b4e585f03c1559fb41f77bf18e810e6bbf726e8
kota <kota200016@icloud.com>
2022-02-18 23:01:16 +0900

    環境変数の追加/メール送信先の変更

--- src/pages/api/sendApplyEmail.ts
+++ src/pages/api/sendApplyEmail.ts
@@ -6,7 +6,7 @@ export default function applyEmailHandler(req: NextApiRequest, res: NextApiRespo
         sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
         const msg = {
-            to: req.body.applicantData.user_email,
+            to: req.body.recruiterData.user_email,
             from: process.env.SENDER_APPLY_EMAIL_ADDRESS,
             subject: `${req.body.subject}への応募メッセージ`,
             html: `<html>こんにちは！<br>
@@ -20,14 +20,13 @@ export default function applyEmailHandler(req: NextApiRequest, res: NextApiRespo
             <br>
             応募者メールアドレス: ${req.body.applicantData.user_email}<br>
             <br>
-            ご不明な点などありましたら、 unwork1201@gmail.com (運営)
-            もしくは 19G083@u-nagano.ac.jp (応募メール担当者)までご連絡ください。</html>`,
+            ご不明な点などありましたら、 ${process.env.ADMIN_EMAIL_ADDRESS} (運営)
+            もしくは ${process.env.SENDER_APPLY_EMAIL_ADDRESS} (応募メール担当者)までご連絡ください。</html>`,
         };
 
         (async () => {
             try {
-                const result = await sgMail.send(msg);
-                console.log(result);
+                await sgMail.send(msg);
                 if (res.status(200)) {
                     return res.send(res.statusCode);
                 } else {
@@ -35,9 +34,6 @@ export default function applyEmailHandler(req: NextApiRequest, res: NextApiRespo
                 }
             } catch (error) {
                 console.error(error);
-                if (error.response) {
-                    console.error(error.response.body);
-                }
             }
         })();
     }
